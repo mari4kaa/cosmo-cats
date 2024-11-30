30e95334380da4286d2e07408237caf778b462a0
Maksym Diachok <maksdyachok2005@gmail.com>
2024-11-30 17:53:30 +0200

    Update mapper

--- src/main/java/com/example/cosmocats/mapper/OrderEntryMapper.java
+++ src/main/java/com/example/cosmocats/mapper/OrderEntryMapper.java
@@ -4,8 +4,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import com.example.cosmocats.domain.Product;
 import com.example.cosmocats.domain.order.OrderEntry;
 import com.example.cosmocats.dto.order.OrderEntryDto;
+import com.example.cosmocats.entities.OrderEntryEntity;
+import com.example.cosmocats.entities.ProductEntity;
 
 @Mapper
 public interface OrderEntryMapper {
@@ -13,9 +16,23 @@ public interface OrderEntryMapper {
         return Mappers.getMapper(OrderEntryMapper.class);
     }
 
-    @Mapping(target = "productId", source = "product.id")
-    OrderEntryDto orderEntryToDto(OrderEntry entry);
+    @Mapping(target = "product", source = "product")
+    OrderEntry entityToDomain(OrderEntryEntity entity);
 
-    @Mapping(target = "product.id", source = "productId")
-    OrderEntry dtoToOrderEntry(OrderEntryDto dto);
+    @Mapping(target = "product", source = "product")
+    OrderEntryEntity domainToEntity(OrderEntry domain);
+
+    default ProductEntity mapProductToEntity(Product product) {
+        if (product == null) return null;
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setId(UuidConverter.uuidToLong(product.getId()));
+        return productEntity;
+    }
+
+    default Product mapProductToDomain(ProductEntity productEntity) {
+        if (productEntity == null) return null;
+        return Product.builder()
+            .id(UuidConverter.longToUuid(productEntity.getId()))
+            .build();
+    }
 }
