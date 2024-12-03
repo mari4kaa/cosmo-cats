4888def1e76a1e7472f4d184563099aacb8140ef
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-12-04 01:27:33 +0200

    Add proper exception handling

--- src/main/java/com/example/cosmocats/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/cosmocats/web/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.cosmocats.web.exception;
 
+import com.example.cosmocats.featuretoggle.exceptions.FeatureNotAvailableException;
+import com.example.cosmocats.service.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +10,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-
-import com.example.cosmocats.featuretoggle.exceptions.FeatureNotAvailableException;
-import com.example.cosmocats.service.exception.ProductCreationException;
 
 import jakarta.validation.ConstraintViolationException;
 import java.net.URI;
@@ -28,6 +27,90 @@ public class GlobalExceptionHandler {
         error.setInstance(URI.create(request.getDescription(false)));
         
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handleCategoryNotFound(
+            CategoryNotFoundException ex, WebRequest request) {
+        
+        ProblemDetail error = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        error.setTitle("Not Found");
+        error.setDetail(ex.getMessage());
+        error.setInstance(URI.create(request.getDescription(false)));
+        
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(ProductCreationException.class)
+    public ResponseEntity<ProblemDetail> handleProductCreationException(
+            ProductCreationException ex, WebRequest request) {
+        
+        ProblemDetail error = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        error.setTitle("Bad Request");
+        error.setDetail(String.format("Validation failed: %s", ex.getMessage()));
+        error.setInstance(URI.create(request.getDescription(false)));
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(CategoryCreationException.class)
+    public ResponseEntity<ProblemDetail> handleCategoryCreationException(
+            CategoryCreationException ex, WebRequest request) {
+        
+        ProblemDetail error = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        error.setTitle("Bad Request");
+        error.setDetail(String.format("Validation failed: %s", ex.getMessage()));
+        error.setInstance(URI.create(request.getDescription(false)));
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(ProductUpdateException.class)
+    public ResponseEntity<ProblemDetail> handleProductUpdateException(
+            ProductUpdateException ex, WebRequest request) {
+        
+        ProblemDetail error = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        error.setTitle("Bad Request");
+        error.setDetail(String.format("Update failed: %s", ex.getMessage()));
+        error.setInstance(URI.create(request.getDescription(false)));
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(CategoryUpdateException.class)
+    public ResponseEntity<ProblemDetail> handleCategoryUpdateException(
+            CategoryUpdateException ex, WebRequest request) {
+        
+        ProblemDetail error = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        error.setTitle("Bad Request");
+        error.setDetail(String.format("Update failed: %s", ex.getMessage()));
+        error.setInstance(URI.create(request.getDescription(false)));
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(ProductDeletionException.class)
+    public ResponseEntity<ProblemDetail> handleProductDeletionException(
+            ProductDeletionException ex, WebRequest request) {
+        
+        ProblemDetail error = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        error.setTitle("Bad Request");
+        error.setDetail(String.format("Deletion failed: %s", ex.getMessage()));
+        error.setInstance(URI.create(request.getDescription(false)));
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(CategoryDeletionException.class)
+    public ResponseEntity<ProblemDetail> handleCategoryDeletionException(
+            CategoryDeletionException ex, WebRequest request) {
+        
+        ProblemDetail error = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        error.setTitle("Bad Request");
+        error.setDetail(String.format("Deletion failed: %s", ex.getMessage()));
+        error.setInstance(URI.create(request.getDescription(false)));
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -75,15 +158,4 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleFeatureNotAvailable(FeatureNotAvailableException ex) {
         return ResponseEntity.notFound().build();
     }
-
-    @ExceptionHandler(ProductCreationException.class)
-    public ResponseEntity<ProblemDetail> handleProductCreationException(ProductCreationException ex, WebRequest request) {
-        ProblemDetail error = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-        error.setTitle("Bad Request");
-        error.setDetail(String.format("Validation failed: %s", ex.getMessage()));
-        error.setInstance(URI.create(request.getDescription(false)));
-
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
-    }
-
 }
