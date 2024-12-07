032ab1be6c025dfe21db183ab09250c766d774b8
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-12-07 15:01:16 +0200

    Documentation and coverage update

--- src/main/java/com/example/cosmocats/CosmoCatsApplication.java
+++ src/main/java/com/example/cosmocats/CosmoCatsApplication.java
@@ -5,6 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Cosmo Cats Marketplace",
+        description = "This is a simple API for Cosmo Cats Marketplace",
+        version = "1.1",
+        license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html")
+    ),
+    servers = {
+    @Server(url = "http://localhost:8080", description = "test server"),
+    }
+)
+
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 @EnableAspectJAutoProxy
 public class CosmoCatsApplication {
